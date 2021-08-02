eb5ab42094395eaba774cb488e84c4b0dada0e0d
Mohammad Rahmani <83430369+MohammadRK2000@users.noreply.github.com>
2021-08-01 22:51:11 -0700

    Update StartIE.js

--- StartIE.js
+++ StartIE.js
@@ -1,8 +1,8 @@
 var navOpenInBackgroundTab = 0x1000;
 var objIE = new ActiveXObject("InternetExplorer.Application");
 
-objIE.Navigate2("http://www.site1.com");
-objIE.Navigate2("http://www.site2.com", navOpenInBackgroundTab);
-objIE.Navigate2("http://www.site3.com", navOpenInBackgroundTab);
+objIE.Navigate2("http://www.google.com");
+objIE.Navigate2("http://www.microsoft.com", navOpenInBackgroundTab);
+objIE.Navigate2("http://www.amazon.com", navOpenInBackgroundTab);
 objIE.Visible = true;
  
